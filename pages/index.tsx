4498508defa7707aa7d6287f6df3c60bcbb3c1d3
monjofight <monjofight@gmail.com>
2023-04-10 21:33:43 +0900

    chakra UIの追加、index.tsxの修正

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,32 @@
-import { GetStaticProps, NextPage } from 'next';
-import Link from 'next/link';
-import fs from 'fs';
-import path from 'path';
+import { Box, VStack, Heading, ListItem, UnorderedList, Link as ChakraLink } from '@chakra-ui/react';
+import { useRouter } from 'next/router';
 
 const Home = () => {
+  const router = useRouter();
+  const handleClick = (href: string) => {
+    router.push(href);
+  };
+
   return (
-    <div>
-      <h1>ページ一覧</h1>
-      <ul>
-        <li>
-          <Link href={`/duck`}>ニワトリ</Link>
-        </li>
-        <li>
-          <Link href={`/mofumofu`}>モフモフ</Link>
-        </li>
-      </ul>
-    </div>
+    <Box textAlign="center" fontSize="xl">
+      <VStack spacing={8} padding={5}>
+        <Heading as="h1" size="2xl" marginBottom="1rem" color="teal.500">
+          ページ一覧
+        </Heading>
+        <UnorderedList spacing={8} textAlign="left" marginLeft="auto" marginRight="auto" listStyleType="none">
+          <ListItem>
+            <ChakraLink href="#" onClick={() => handleClick('/duck')} padding="8px" borderRadius="md" backgroundColor="pink.200" _hover={{ textDecoration: 'none', backgroundColor: 'pink.300' }}>
+              ニワトリ
+            </ChakraLink>
+          </ListItem>
+          <ListItem>
+            <ChakraLink href="#" onClick={() => handleClick('/mofumofu')} padding="8px" borderRadius="md" backgroundColor="purple.200" _hover={{ textDecoration: 'none', backgroundColor: 'purple.300' }}>
+              モフモフ
+            </ChakraLink>
+          </ListItem>
+        </UnorderedList>
+      </VStack>
+    </Box>
   );
 };
 
